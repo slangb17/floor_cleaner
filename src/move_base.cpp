f99190bf3f871780acb93e88bb284e034f7fbac1
Simon Mouritsen Langbak <slangb17@student.aau.dk>
2018-11-30 23:47:24 +0100

    Speed adjustments

--- src/move_base.cpp
+++ src/move_base.cpp
@@ -2,8 +2,17 @@
 #include "geometry_msgs/Twist.h"
 #include "sensor_msgs/LaserScan.h"
 
-void scan_callback(){
+sensor_msgs::LaserScan laser_msg;
+std::vector<float> laser_ranges;
+geometry_msgs::Twist velocity_msg;
+
+void scan_callback(const sensor_msgs::LaserScan::ConstPtr &scan_msg){
   
+  laser_msg = *scan_msg;
+
+  laser_ranges = laser_msg.ranges;
+
+  ROS_INFO("%d", laser_ranges.size());
 }
 
 int main(int argc, char **argv)
@@ -11,14 +20,21 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "move_base");
 
   // Node handler
-  ros::NodeHandle nh;
+  ros::NodeHandle n;
 
   // Publisher
-  ros::Publisher cmd_vel = nh.advertise<geometry_msgs::Twist> 
+  ros::Publisher cmd_vel = n.advertise<geometry_msgs::Twist> 
                                                  ("/cmd_vel_mux/input/navi", 100);
   
   // Subscriber
-  ros::Subscriber laser_scan = nh.subscribe ("/scan", 1000, scan_callback);
+  ros::Subscriber laser_scan = n.subscribe("/scan", 1000, scan_callback);
 
+  while(ros::ok()){
+
+
+
+    ros::spinOnce();
+
+  }
   return 0;
 }
