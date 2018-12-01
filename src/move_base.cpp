cfdf255988cdff120b3a74cd52eb13d932f1ed0a
Egil <egilsagdahl9@gmail.com>
2018-12-01 18:49:44 +0100

    added comments

--- src/move_base.cpp
+++ src/move_base.cpp
@@ -25,7 +25,7 @@ using namespace geometry_msgs;
 
 // The published is defined here as it's used in the scan_callback function:
 ros::Publisher cmd_vel;
-ros::Publisher cleaning
+ros::Publisher cleaning;
 
 //array for x and y coordinates
 double x_cord [4];
@@ -106,7 +106,7 @@ void scan_callback(const sensor_msgs::LaserScan::ConstPtr &scan_msg)
 
   // The sections are printed in the terminal:
   ROS_INFO("Left: %f, center: %f, right: %f", laser_ranges_left,
-           laser_ranges_center, laser_ranges_right);
+  laser_ranges_center, laser_ranges_right);
 
   // The code inside the if-statement will only be run if a corner has NOT been found:
   if (cornerFound != true)
@@ -194,94 +194,94 @@ void scan_callback(const sensor_msgs::LaserScan::ConstPtr &scan_msg)
       }
     }
 
-  if (cornerFound == true)
-  {
-corner_saver();
-
-    ROS_WARN("I FOUND A CORNER: %d",counter);
-    cornerFound = false;
-//    velocity_msg.linear.x = 0;
-//    velocity_msg.angular.z = 0;
-  }
-
-  cmd_vel.publish(velocity_msg);
-}
-
-
-void corner_saver(){
-  // checking time since last run, this is to secure the robot does not run this function multiple times in same corner
-  if(clock()-old_time<20000000){return;}
-old_time= clock();
-
- geometry_msgs::TransformStamped transformStamped;
-ROS_INFO("TIME: %d",old_time);
- // trying to obtain coordinates, if it fails it will just send a warning and wait a second.
-  try
-  {
-    transformStamped = tfBuffer.lookupTransform( "map","base_footprint",
-                                                ros::Time(0));
-  }
-  catch (tf2::TransformException &ex)
-  {
-    ROS_WARN("%s", ex.what());
-    ros::Duration(1.0).sleep();
-
-  }
-
-
-  //adding coordinates in the x and y array
-  x_cord[counter] = fabs(transformStamped.transform.translation.x);
-  y_cord[counter] = fabs(transformStamped.transform.translation.y);
-
-  //incrementing to assign a place in the array
-  counter++;
-}
-
-
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "move_base");
-
-
-  // Node handler
-  ros::NodeHandle n;
-  tf2_ros::TransformListener tfListener(tfBuffer);
-  old_time= clock();
-
-  // Publisher
-  cmd_vel = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 100);
-  cleaning = n.advertise<std_msgs::Float64MultiArray>("/cleaning_points", 100);
-
-
-  // Subscriber
-  ros::Subscriber laser_scan = n.subscribe("/scan", 1000, scan_callback);
-
-  while (ros::ok())
-  {
-    ros::spinOnce();
-    //checks to see if conter if more than 2. If counter is 3, the robot has obtained 4 coordinatesets for 4 different corners.
-    if(counter>2)
+    if (cornerFound == true)
     {
-      //initializing an array of the type float64
-      std_msgs::Float64MultiArray tmp_array;
-      //everything in the array is set to 0
-      tmp_array.data.clear();
+      corner_saver();
 
-      //counter to go throught the arrays
-      for(int i=0;i<=counter;i++){
-        //using the push_back function to push first x than y to the array
-        tmp_array.data.push_back(x_cord[i]);
-        tmp_array.data.push_back(y_cord[i]);
-      }
-      //publish the array
-      cleaning.publish(tmp_array);
-      //sleep so the new node can secure the data sendt from this node before this node shutsdown
-      ros:: Duration(10).sleep();
-      //shutsdown node
-      ros:: shutdown();
+      ROS_WARN("I FOUND A CORNER: %d",counter);
+      cornerFound = false;
+      //    velocity_msg.linear.x = 0;
+      //    velocity_msg.angular.z = 0;
+    }
+
+    cmd_vel.publish(velocity_msg);
+  }
+
+
+  void corner_saver(){
+    // checking time since last run, this is to secure the robot does not run this function multiple times in same corner
+    if(clock()-old_time<20000000){return;}
+    old_time= clock();
+
+    geometry_msgs::TransformStamped transformStamped;
+    ROS_INFO("TIME: %d",old_time);
+    // trying to obtain coordinates, if it fails it will just send a warning and wait a second.
+    try
+    {
+      transformStamped = tfBuffer.lookupTransform( "map","base_footprint",
+      ros::Time(0));
+    }
+    catch (tf2::TransformException &ex)
+    {
+      ROS_WARN("%s", ex.what());
+      ros::Duration(1.0).sleep();
+
     }
 
 
+    //adding coordinates in the x and y array
+    x_cord[counter] = fabs(transformStamped.transform.translation.x);
+    y_cord[counter] = fabs(transformStamped.transform.translation.y);
+
+    //incrementing to assign a place in the array
+    counter++;
   }
-  return 0;
-}
+
+
+  int main(int argc, char **argv)
+  {
+    ros::init(argc, argv, "move_base");
+
+
+    // Node handler
+    ros::NodeHandle n;
+    tf2_ros::TransformListener tfListener(tfBuffer);
+    old_time= clock();
+
+    // Publisher
+    cmd_vel = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 100);
+    cleaning = n.advertise<std_msgs::Float64MultiArray>("/cleaning_points", 100);
+
+
+    // Subscriber
+    ros::Subscriber laser_scan = n.subscribe("/scan", 1000, scan_callback);
+
+    while (ros::ok())
+    {
+      ros::spinOnce();
+      //checks to see if conter if more than 2. If counter is 3, the robot has obtained 4 coordinatesets for 4 different corners.
+      if(counter>2)
+      {
+        //initializing an array of the type float64
+        std_msgs::Float64MultiArray tmp_array;
+        //everything in the array is set to 0
+        tmp_array.data.clear();
+
+        //counter to go throught the arrays
+        for(int i=0;i<=counter;i++){
+          //using the push_back function to push first x than y to the array
+          tmp_array.data.push_back(x_cord[i]);
+          tmp_array.data.push_back(y_cord[i]);
+        }
+        //publish the array
+        cleaning.publish(tmp_array);
+        //sleep so the new node can secure the data sendt from this node before this node shutsdown
+        ros:: Duration(10).sleep();
+        //shutsdown node
+        ros:: shutdown();
+      }
+
+
+    }
+    return 0;
+  }
